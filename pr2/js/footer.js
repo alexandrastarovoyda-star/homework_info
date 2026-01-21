3cdc37482f1f0725369ec1b10de039199c0cece3
Oleksandra <alexandra.starovoyda@gmail.com>
2026-01-21 15:32:31 +0200

    pr2

---
+++ pr2/js/footer.js
@@ -0,0 +1,3 @@
+const footer = document.createElement('footer');
+footer.innerHTML  = '<p class="cn">Kremenchuk <br> 2026 <br> <a href="mailto:starovoida.o.a@kremenchuk.ukr.education?subject=Рекламна_пропозиція">starovoida.o.a@kremenchuk.ukr.education</a></p>'
+document.body.appendChild(footer)
